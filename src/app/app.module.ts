81defe3c52474711caee0f12bc8b7896ee0b7dfd
yoave11 <yoave11@yahoo.com>
2017-06-14 21:03:07 +0300

    abode xd prototype + navigation styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./home/home.component";
+import {HeaderComponent} from "./header/header.component";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
+    ,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
