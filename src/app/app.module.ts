e6a15c23c1091e7007c9c85a354568529909d53b
yoave11 <yoave11@yahoo.com>
2017-06-24 15:45:54 +0300

    home page - added latest news section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,13 @@ import {HeaderComponent} from "./header/header.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { FooterComponent } from './footer/footer.component';
 import { CarouselComponent } from './home/carousel/carousel.component';
-
+import {Ng2ParallaxScrollDirective} from "ng2-parallax-scroll";
+import {Parallax, ParallaxConfig} from "ng2-parallax/dist/ts/commonjs/parallax.directive";
+// import {Parallax} from "ng2-parallax/dist/ts/commonjs/parallax.directive";
+// import {NguiModule} from '@ngui/ngui'
+// import {} from "ng2-parallax";
+// import {ng2Parallax} from '../../node_modules/ang2-parallax'
+;
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,15 +22,20 @@ import { CarouselComponent } from './home/carousel/carousel.component';
     HeaderComponent,
     FooterComponent,
     CarouselComponent,
+    Parallax,
+
+
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
-    ,
+    HttpModule,
+    // Parallax,
     BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+
 })
 export class AppModule { }
